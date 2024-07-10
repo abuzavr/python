e588e1ddd6b107a51cf48587a933e64afebd7e16
abuzavr <jakzor@mail.ru>
2024-07-11 00:38:37 +0300

    Create module_3_2.py

---
+++ module_3_2.py
@@ -0,0 +1,19 @@
+def send_email(message, recipient, *, sender = "university.help@gmail.com"):
+    true_domains = ['.com', '.ru', '.net']
+    def validate_email(email):
+        return '@' in email and any(email.endswith(domain) for domain in true_domains)
+    if not validate_email(recipient) or not validate_email(sender):
+        print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
+    elif sender == recipient:
+        print("Нельзя отправить письмо самому себе!")
+    elif sender == "university.help@gmail.com":
+        print(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient}")
+    else:
+        print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}")
+
+send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
+send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
+send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
+
+
