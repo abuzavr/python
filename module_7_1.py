62bde565f4831077a63e980c948c02cc0c60937c
abuzavr <jakzor@mail.ru>
2024-10-20 15:23:02 +0300

    Create module_7_1.py

---
+++ module_7_1.py
@@ -0,0 +1,39 @@
+class Product:
+    def __init__(self, name, weight, category):
+        self.name = name
+        self.weight = weight
+        self.category = category
+
+    def __str__(self):
+        return f'{self.name}, {self.weight}, {self.category}'
+
+
+class Shop:
+    def __init__(self):
+        self.__file_name = 'products.txt'
+
+    def get_products(self):
+        with open(self.__file_name, 'r', encoding='utf-8') as file:
+            return file.read()
+
+    def add(self, *products):
+        current_products = self.get_products().split('\n')
+        with open(self.__file_name, 'a', encoding='utf-8') as file:
+            for product in products:
+                if product.__str__() not in current_products:
+                    file.write(product.__str__() + '\n')
+                else:
+                    print(f'Продукт {product.name}, {product.weight}, {product.category} уже есть в магазине')
+
+
+
+
+s1 = Shop()
+p1 = Product('Potato', 50.5, 'Vegetables')
+p2 = Product('Spaghetti', 3.4, 'Groceries')
+p3 = Product('Potato', 5.5, 'Vegetables')
+
+print(p2)  # __str__
+
+s1.add(p1, p2, p3)
+print(s1.get_products())
