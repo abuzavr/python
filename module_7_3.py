a9122fff993ebed5764668f47c50008daec65dd8
abuzavr <jakzor@mail.ru>
2024-11-10 05:27:14 +0300

    +

--- module_7_3.py
+++ module_7_3.py
@@ -1,25 +1,43 @@
 class WordsFinder:
-
-    def __init__(self, filename):
-        #сохраняем имена файлов в атрибут
-        self.filename = filename
+    def __init__(self, *file_names):
+        self.file_names = file_names
 
     def get_all_words(self):
-        #словарь для хранения слов
         all_words = {}
+        punctuation = [',', '.', '=', '!', '?', ';', ':', ' - ']
 
-        for file_name in self.filename:
-                try:
+        for file_name in self.file_names:
             with open(file_name, 'r', encoding='utf-8') as file:
                 text = file.read().lower()
-                for punctuation in [',', '.', '=', '!', '?', ';', ':', ' - ']:
-                    text = text.replace(punctuation, ' ')
-                # Разбиваем текст на слова
+
+                for punct in punctuation:
+                    text = text.replace(punct, ' ')
+
                 words = text.split()
-                # Добавляем слова файла в словарь
                 all_words[file_name] = words
-            except FileNotFoundError:
-            print(f"Файл {file_name} не найден.")
-            all_words[file_name] = []
 
+        return all_words
 
+    def find(self, word):
+        word = word.lower()
+        all_words = self.get_all_words()
+        result = {}
+
+        for file_name, words in all_words.items():
+            position = words.index(word) if word in words else None
+            result[file_name] = position + 1
+
+        return result
+
+    def count(self, word):
+        word = word.lower()
+        all_words = self.get_all_words()
+        result = {file_name: words.count(word) for file_name, words in all_words.items()}
+
+        return result
+
+# Пример использования
+finder2 = WordsFinder('test_file.txt')
+print(finder2.get_all_words()) # Все слова
+print(finder2.find('TEXT')) # 3 слово по счёту
+print(finder2.count('teXT')) # 4 слова teXT в тексте всего
